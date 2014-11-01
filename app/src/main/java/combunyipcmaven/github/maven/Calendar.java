f0c432140587aeb4252134573dc5585c38c5b85a
H. David Zhu <huanzhendavidzhu@gmail.com>
2014-11-01 19:47:42 -0400

    Basic calendar works.

--- app/src/main/java/combunyipcmaven/github/maven/Calendar.java
+++ app/src/main/java/combunyipcmaven/github/maven/Calendar.java
@@ -8,14 +8,14 @@ import android.widget.Button;
 
 public class Calendar extends Activity {
 
-    Intent openCalendarViewer
+    Intent openCalendarViewer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.time_choose);
-        openCalendarViewer = new Intent("com.combunyipcmaven.github.maven.Calendar");
+//        openCalendarViewer = new Intent("com.combunyipcmaven.github.maven.Calendar");
 
         Button finishCalendarButton = (Button) findViewById(R.id.bt_finish_calendar);
         finishCalendarButton.setOnClickListener(new View.OnClickListener() {
